6b7c2ca2e9bb389fc07916f3e06f4a2ce57901c2
WeiAnAn <j22461615@gmail.com>
2018-03-28 09:28:10 +0800

    setup db

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const knex = require('knex');
+const db = knex({
+  client: process.env.DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+});
+
+module.exports = db;
