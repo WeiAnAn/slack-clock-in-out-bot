1c2ea54ce65acf42ca9bf024cedf620c8fbd0822
WeiAnAn <j22461615@gmail.com>
2018-04-14 23:25:26 +0800

    update eslint rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: 'eslint:recommended',
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
   },
