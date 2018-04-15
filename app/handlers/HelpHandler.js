cfc1ad7c6e0f31e6e7da03d7f2a7c91a001225df
WeiAnAn <j22461615@gmail.com>
2018-04-15 20:39:06 +0800

    add missing \n

--- app/handlers/HelpHandler.js
+++ app/handlers/HelpHandler.js
@@ -3,14 +3,14 @@ const formatMsg = require('../utils/formatMsg');
 function HelpHandler(ctx) {
   return ctx.sendText(
     formatMsg(
-      'you can use those command' +
-        'clock in:`in`' +
-        'clock out: `out`' +
-        'check current status: `status`' +
-        'list record: `list (<page>)`' +
-        'edit record: `edit <(record_id|today)> <type(in|out)> <date(YYYY-MM-DD)> <time(HH:mm:SS)>`' +
-        'export all record: `export`' +
-        'export month record: `export month <year> <month>`' +
+      'you can use those command\n' +
+        'clock in:`in`\n' +
+        'clock out: `out`\n' +
+        'check current status: `status`\n' +
+        'list record: `list (<page>)`\n' +
+        'edit record: `edit <(record_id|today)> <type(in|out)> <date(YYYY-MM-DD)> <time(HH:mm:SS)>`\n' +
+        'export all record: `export`\n' +
+        'export month record: `export month <year> <month>`\n' +
         'export record in range: `export between <start date(YYYY-MM-DD)> <end date(YYYY-MM-DD)>`',
       ctx.event.message.user
     )
